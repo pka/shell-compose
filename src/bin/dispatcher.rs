5ffd13b049ea4eebff54dfb01cf05f28a4379225
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2024-09-17 17:18:39 +0200

    Merge cli and server into single binary

--- src/bin/dispatcher.rs
+++ src/bin/dispatcher.rs
@@ -1,6 +1,55 @@
 use process_dispatcher::*;
+use std::env;
+use std::process::{Child, Command};
+use std::time::Duration;
 
-fn main() {
+pub struct DispatcherProc {
+    pub proc: Child,
+}
+
+impl DispatcherProc {
+    pub fn new() -> DispatcherProc {
+        let exe = env::current_exe().unwrap();
+        DispatcherProc {
+            proc: Command::new(exe)
+                .arg("serve")
+                // .stdin(Stdio::piped())
+                // .stdout(Stdio::piped())
+                .spawn()
+                .unwrap(),
+        }
+    }
+}
+
+// impl Drop for DispatcherProc {
+//     fn drop(&mut self) {
+//         self.proc.kill().unwrap();
+//     }
+// }
+
+fn cli() {
+    // if ipc_client_connect(SOCKET_NAME).is_err() {
+    if send_ipc_message(SOCKET_NAME, &Message::Hello).is_err() {
+        eprintln!("Starting dispatcher");
+        let _ = DispatcherProc::new();
+        std::thread::sleep(Duration::from_secs(1));
+    }
+
+    let text = Message::Text {
+        text: "Hello from client!".to_string(),
+    };
+
+    let ping = Message::Ping;
+
+    send_ipc_message(SOCKET_NAME, &text).expect("Failed to connect to socket");
+
+    let response: Message =
+        send_ipc_query(SOCKET_NAME, &ping).expect("Failed to connect to socket");
+
+    dbg!(response);
+}
+
+fn run_server() {
     start_ipc_server(
         SOCKET_NAME,
         |message: Message| match message {
@@ -16,4 +65,14 @@ fn main() {
     .expect("Failed to start ipc listener")
     .join()
     .expect("Failed to join server thread");
+}
+
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+
+    if let Some("serve") = args.get(1).map(|s| s.as_str()) {
+        run_server()
+    } else {
+        cli()
+    }
 }
